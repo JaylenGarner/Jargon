4d561fd579bef65e0367aacbcf0e0e3ad0b7f1bb
Jaylen Garner <jaylen.garner@outlook.com>
2023-01-08 10:24:51 -0500

    Configured get server by ID API route

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -1,11 +1,12 @@
-# from flask import Blueprint, request, jsonify
-# from app.models import db, User, Server, Message, Channel, server_member
-# from flask_login import current_user
+from flask import Blueprint, request, jsonify
+from app.models import db, User, Server, Message, Channel, server_member
+from flask_login import current_user, login_required
 
-# server_routes = Blueprint('server', __name__)
+server_routes = Blueprint('servers', __name__)
 
-# @server_routes.route('')
-#     def get_all_servers():
-#         servers = Server.query.all()
+@server_routes.route('/<int:id>')
+@login_required
+def get_server_by_id(id):
 
-#         res = {server.id: }
+    server = Server.query.get(id)
+    return server.to_dict()
