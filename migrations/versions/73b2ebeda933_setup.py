a73a139967aefe022b757096615f578d16d6c62c
Jaylen Garner <jaylen.garner@outlook.com>
2023-01-14 13:26:55 -0800

    Migration adjustments for render redeploy

--- migrations/versions/73b2ebeda933_setup.py
+++ migrations/versions/73b2ebeda933_setup.py
@@ -1,12 +1,15 @@
 """setup
 
 Revision ID: 73b2ebeda933
-Revises: 
+Revises:
 Create Date: 2023-01-14 11:43:14.297005
 
 """
 from alembic import op
 import sqlalchemy as sa
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
@@ -60,6 +63,12 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE servers SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE channels SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE server_members SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE messages SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
