13b70394d1e38bc18cf85c14cdd00b64ca683bc5
Jaylen Garner <jaylen.garner@outlook.com>
2023-05-01 15:52:34 -0400

    Social icons added to home page

--- react-app/src/components/HomePage.js
+++ react-app/src/components/HomePage.js
@@ -15,13 +15,21 @@ const HomePage = () => {
 
         return (
                 <div className='home-container'>
-
-                    {!user &&
+                    <div className='social-links'>
+                    <a href='https://github.com/JaylenGarner' target="_blank">
+                    <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" class="social-logo"></img>
+                    </a>
+                    <a href='https://www.linkedin.com/in/jaylen-garner-00a252205/' target="_blank">
+                    <img src='https://st4.depositphotos.com/18850080/21051/v/600/depositphotos_210516622-stock-illustration-linkedin-logo-icon-social-media.jpg' className='social-logo'></img>
+                    </a>
+                    <a href='https://wellfound.com/u/jaylen-garner' target="_blank">
+                    <img src='https://www.logolynx.com/images/logolynx/89/890e037ba670b615fb4b26106f253cad.jpeg' className='social-logo'></img>
+                    </a>
+                    </div>
                     <div className='home-header-container'>
                     <h1 className='home-header'>Jargon</h1>
                     <img src='https://jargon-app-images.s3.amazonaws.com/jargon-favicon.png' className='jargon-logo'></img>
                     </div>
-                        }
                     <div className='github-logo-container'>
                     <a href='https://github.com/JaylenGarner/Disbored'>
                     </a>
